5479b2bc9ea91b0e7c38a4d0e9e8c772478bd39c
engineeralok <engineeralok07@gmail.com>
2023-12-24 18:52:34 +0530

    Added: assets and common code from sample code of debestech

---
+++ chatter_box/lib/common/apis/contact.dart
@@ -0,0 +1,13 @@
+import 'package:chatter_box/common/entities/entities.dart';
+import 'package:chatter_box/common/utils/utils.dart';
+
+class ContactAPI {
+  /// 翻页
+  /// refresh 是否刷新
+  static Future<ContactResponseEntity> post_contact() async {
+    var response = await HttpUtil().post(
+      'api/contact',
+    );
+    return ContactResponseEntity.fromJson(response);
+  }
+}
--- chatter_box/macos/Flutter/GeneratedPluginRegistrant.swift
+++ chatter_box/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,22 @@
 import FlutterMacOS
 import Foundation
 
+import cloud_firestore
+import firebase_core
+import firebase_messaging
+import flutter_local_notifications
+import package_info_plus
+import path_provider_foundation
+import shared_preferences_foundation
+import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
